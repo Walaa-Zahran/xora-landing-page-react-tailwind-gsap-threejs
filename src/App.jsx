5e04d7d0db5edb912edf1f670e67ba8e7f93cb13
walaa-zahran <walaazahran40@gmail.com>
2025-12-16 11:04:20 +0200

    footer , testimonials ,  download sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Download from "./sections/Download";
 import FAQs from "./sections/FAQs";
 import Features from "./sections/features";
+import Footer from "./sections/Footer";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Pricing from "./sections/Pricing";
+import Testimonials from "./sections/Testimonials";
 
 const App = () => {
   return (
@@ -12,6 +15,9 @@ const App = () => {
       <Features />
       <Pricing />
       <FAQs />
+      <Testimonials />
+      <Download />
+      <Footer />
     </main>
   );
 };
